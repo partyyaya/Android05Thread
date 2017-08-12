62f2a9f3dab1e1826f034266195e1da870ae0d5b
LBJ <LBJ>
2017-08-12 11:37:37 +0800

    20170812

--- app/src/main/java/tw/ming/app/helloworid/mythread/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mythread/MainActivity.java
@@ -1,13 +1,79 @@
 package tw.ming.app.helloworid.mythread;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mesg;
+    private UIHandler handler;
+    private Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        mesg = (TextView)findViewById(R.id.mesg);
+    }
+    public void test1(View view){
+        MyThread01 mt1 = new MyThread01();
+        MyThread01 mt2 = new MyThread01();
+        mt1.start(); mt2.start();
+    }
+    public void test2(View view){
+    timer.schedule(new MyTask(),1000,500);
+    }
+    public void test3(View view){
+
+    }
+
+    private class MyThread01 extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 20; i++) {
+                Log.i("ming", "i=" + i);
+                //mesg.setText(" i = " + i);
+                handler.sendEmptyMessage(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    @Override
+    public void finish() {
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+        super.finish();
+    }
+
+    private class MyTask extends TimerTask {
+        private int i;
+        @Override
+        public void run() {
+            i++;
+            Log.i("brad", "i = " + i);
+            handler.sendEmptyMessage(i);
+        }
+    }
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mesg.setText("i="+msg.what);
+        }
     }
 }
